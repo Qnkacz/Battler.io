f1c000a36c6b5900ffff33246f54d6c806248c4c
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2021-10-26 22:05:51 +0200

    Configured project

---
+++ Battler.io/Assets/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Transform;
+    public Camera Camera;
+    void Start()
+    {
+        
+    }
+}
