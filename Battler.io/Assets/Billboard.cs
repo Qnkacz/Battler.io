d4f8771e164fa6e08fcde8912f62df65353ee2e0
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2021-12-03 17:10:56 +0100

    Fixed

--- Battler.io/Assets/Billboard.cs
+++ Battler.io/Assets/Billboard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public Transform Cam;
+
+    private void Awake()
+    {
+        if (Cam == null) Cam = Camera.main!.transform;
+    }
 
     void LateUpdate()
     {
