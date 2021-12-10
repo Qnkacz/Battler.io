644465dd5866661eb06996d17aa65864f0a65f5d
Qnkacz <37179465+Qnkacz@users.noreply.github.com>
2021-12-10 16:59:15 +0100

    Added armyHelper

--- Battler.io/Assets/Scripts/Helper/ArmyHelper.cs
+++ Battler.io/Assets/Scripts/Helper/ArmyHelper.cs
@@ -1,13 +1,54 @@
+using System;
+using System.Linq;
 using Battle.Unit;
+using DefaultNamespace;
 
 namespace Helper
 {
+    /// <summary>
+    /// Helper to get info about available armies
+    /// </summary>
     public static class ArmyHelper
     {
+        /// <summary>
+        /// Returns the max unit amount of chosen faction and type
+        /// </summary>
+        /// <param name="faction"> Chosen faction</param>
+        /// <param name="type"> Chosen unit attack type</param>
+        /// <returns>int</returns>
         public static int GetUnitMaxAmount(UnitFaction faction, UnitAttackType type)
         {
-            var returnValue = 0;
-            return 0;
+            var armyInfo = BattleMapGeneratorManager.GameManager.UnitGenerator.ArmyList.First(info => info.Faction==faction);
+            var value = type switch
+            {
+                UnitAttackType.Melee => armyInfo.TroopAmount,
+                UnitAttackType.Range => armyInfo.ArcherAmount,
+                UnitAttackType.Flying => armyInfo.FlyingAmount,
+                _ => throw new Exception("Wrong type")
+            };
+            
+            return value;
+        }
+
+        /// <summary>
+        /// Returns the factions unit cap
+        /// </summary>
+        /// <param name="faction">Chosen faction</param>
+        /// <returns>int</returns>
+        public static int GetFactionMaxUnits(UnitFaction faction)
+        {
+            var armyInfo = BattleMapGeneratorManager.GameManager.UnitGenerator.ArmyList.First(info => info.Faction==faction);
+            return armyInfo.TotalAmount;
+        }
+
+        /// <summary>
+        /// Returns army infro from the chosen faction
+        /// </summary>
+        /// <param name="faction">Chosen faction</param>
+        /// <returns>ArmyInfo</returns>
+        public static ArmyInfo GetArmyInfo(UnitFaction faction)
+        {
+            return BattleMapGeneratorManager.GameManager.UnitGenerator.ArmyList.First(info => info.Faction==faction);
         }
     }
 }
--- Battler.io/Assets/Scripts/Helper/BoundsHelper.cs
+++ Battler.io/Assets/Scripts/Helper/BoundsHelper.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 namespace Helper
 {
+    /// <summary>
+    /// Helper To get info about in map bounds
+    /// </summary>
     public static class BoundsHelper
     {
+        /// <summary>
+        /// Gets Human placement bounds
+        /// </summary>
+        /// <returns></returns>
         public static Bounds GetHumanBounds()
         {
             return BattleMapGeneratorManager.GameManager.BattleMap.PlayerUnitPlacementBounds;
         }
+        /// <summary>
+        /// Gets undead placement bounds
+        /// </summary>
+        /// <returns></returns>
         public static Bounds GetUndeadBounds()
         {
             return BattleMapGeneratorManager.GameManager.BattleMap.AIUnitPlacementBounds;
+        }
+
+        /// <summary>
+        /// Gets obstacle bounds
+        /// </summary>
+        /// <returns></returns>
+        public static Bounds GetObstacleBounds()
+        {
+            return BattleMapGeneratorManager.GameManager.BattleMap.ObstacleSpawnBounds;
         }
     }
 }
